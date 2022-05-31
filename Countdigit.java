161257d621fb8c4dfbabebf5f82f9cc1d19d6967
Aaditya kumar yadav <94703172+aditya7250@users.noreply.github.com>
2022-05-31 08:58:43 +0530

    Add files via upload

---
+++ Countdigit.java
@@ -0,0 +1,21 @@
+package Dsa_problem;
+import java.util.Scanner;
+public class Countdigit {
+		    public static void main(String args[]) {
+		        Scanner sc = new Scanner (System.in);
+		        int n = sc.nextInt();
+		        int d = sc.nextInt();
+		        int count = 0;
+		        while (n > 0) {
+		             int rem = n % 10;
+		             if ( rem == d) {
+		                  count++;
+		             }
+		                n /=10;
+		        } 
+		            System.out.println(count) ;
+		             
+		             }
+		        }
+
+		    
