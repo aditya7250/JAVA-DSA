c144726b0e94baba190b5acee5f424d8ff1fa459
Aaditya kumar yadav <94703172+aditya7250@users.noreply.github.com>
2022-06-02 01:01:22 +0530

    Add files via upload

---
+++ HollowRombusLadder.java
@@ -0,0 +1,26 @@
+package Dsa_problem;
+import java.util.Scanner;
+public class HollowRombusLadder {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int rows = sc.nextInt();		
+		for (int i = 1 ; i <= rows; i++ ) 
+		{
+			for (int j = 1 ; j <= rows - i; j++ ) 
+			{
+				System.out.print(" ");
+			}
+			for (int k = 1 ; k <= rows; k++ ) 
+			{
+				if (i == 1 || i == rows || k == 1 || k == rows) {
+					System.out.print("*");
+				}
+				else {
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+	}
+}
