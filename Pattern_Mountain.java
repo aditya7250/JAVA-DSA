baab188d90acdfc690104a09929d65df2da44004
Aaditya kumar yadav <94703172+aditya7250@users.noreply.github.com>
2022-06-02 00:58:28 +0530

    Add files via upload

---
+++ Pattern_Mountain.java
@@ -0,0 +1,37 @@
+package Dsa_problem;
+import java.util.*;
+public class Pattern_Mountain {
+	
+	public static void main(String[] args) {    
+    Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int nod = 1;
+        int count = 1;
+        int sp = n+1;
+
+        for(int i = 1; i<=n; i++){
+
+            for(int j = 1; j<=nod; j++){
+                System.out.print(count+"\t");
+                count++;
+            }
+            
+            if(i==n){
+                count--;
+                nod--;                             
+            }
+            
+            for(int j = 1; j<=sp; j++){
+                System.out.print("\t");
+            }
+            
+            for(int j = nod; j>=1; j--){
+                count--;
+                System.out.print(count+"\t");                
+            }
+            nod++;
+            sp-=2;
+            System.out.println();
+        }
+    }
+}
