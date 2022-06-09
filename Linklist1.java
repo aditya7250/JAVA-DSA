dc7f417be140232b96042d177aa1639bdc16699a
Aaditya kumar yadav <94703172+aditya7250@users.noreply.github.com>
2022-06-09 13:36:41 +0530

    Add files via upload

---
+++ Linklist1.java
@@ -0,0 +1,13 @@
+package JavaDsa9;
+import java.util.LinkedList;
+public class Linklist1 {
+
+	public static void main(String[] args) {
+
+    LinkedList<String> animals = new LinkedList<>();
+    animals.add("Dog");
+    animals.add("Cat");
+    animals.add("Cow");
+    System.out.println("LinkedList: " + animals);
+  }
+}
