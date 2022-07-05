7da72afcb5a6f365ab2b7bd1f67367176a56ef45
Aaditya kumar yadav <94703172+aditya7250@users.noreply.github.com>
2022-07-05 23:09:25 +0530

    Add files via upload

---
+++ Testarray.java
@@ -0,0 +1,16 @@
+package Eassy_java21;
+
+public class Testarray {
+
+ 
+public static void main(String args[]){  
+int a[]=new int[5];//declaration and instantiation  
+a[0]=10;//initialization  
+a[1]=20;  
+a[2]=30;  
+a[3]=40;  
+a[4]=50;  
+//traversing array  
+for(int i=0;i<5;i++)//length is the property of array  
+System.out.println(a[i]);  
+}}  
