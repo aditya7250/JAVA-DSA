ff09a9018d4571852412c2ede8e5743f1b0cd5eb
Aaditya kumar yadav <94703172+aditya7250@users.noreply.github.com>
2022-07-25 14:41:47 +0530

    Add files via upload

---
+++ ArrayMinValue.java
@@ -0,0 +1,22 @@
+
+public class ArrayMinValue {
+
+	public static void main(String[] args) {
+		int[] arr = { 10, -3, 90, 7, -9, 79, -18, 902 };
+		System.out.println(Minimum(arr));
+
+	}
+
+	public static int Minimum(int[] arr) {
+
+		int min = arr[0];
+		for (int i = 1; i < arr.length; i++) {
+			if (arr[i] < min) {
+				min = arr[i];
+			}
+
+		}
+		return min;
+
+	}
+}
