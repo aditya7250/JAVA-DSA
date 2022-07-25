320a60d8c2a2639a020546e89531f515bf1e0b58
Aaditya kumar yadav <94703172+aditya7250@users.noreply.github.com>
2022-07-26 01:08:55 +0530

    Add files via upload

--- ReverseArray.java
+++ ReverseArray.java
@@ -1,31 +1,22 @@
-package Dsa_problem;
-import java.util.*;
+
 public class ReverseArray {
-
-	public static void main(String[] args) {
-
-       Scanner sc=new Scanner(System.in);
-        int N=sc.nextInt();
-        int [] arr=new int[N];
-        for(int i=0;i<arr.length;i++)//change( we have to run for 0 to N-1)
-        {
-             arr[i]=sc.nextInt();
-        }
-        Reverse(arr);
-        for(int k=0;k<arr.length;k++){//traverse from 0 to arr.length-1
-             System.out.println(arr[k]);//print in next line
-        }
-    }
-    public static void Reverse(int [] arr)
-    {
-        int i=0;
-        int j=arr.length-1;
-        while(i<j){
-            int temp=arr[i];
-            arr[i]=arr[j];
-            arr[j]=temp;
-            i++;
-            j--;
-        }//added this bracket
-    }
-}
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			int[] arr = { 23, 4, 5, 67, 89, 10, 12 };
+			ReverseArray(arr);
+			for (int i = 0; i < arr.length; i++) {
+				System.out.print(arr[i] + " ");
+			}
+		}
+		public static void ReverseArray(int[] arr) {
+			int i = 0;
+			int j = arr.length - 1;
+			while (i < j) {
+				int temp = arr[i];
+				arr[i] = arr[j];
+				arr[j] = temp;
+				i++;
+				j--;
+			}
+		}
+	}
