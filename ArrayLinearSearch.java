2d6803fc3c5cdf871e34371a9249f9b028fe73bc
Aaditya kumar yadav <94703172+aditya7250@users.noreply.github.com>
2022-07-25 14:41:18 +0530

    Add files via upload

---
+++ ArrayLinearSearch.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class ArrayLinearSearch {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] arr = new int[n];
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = sc.nextInt();
+
+		}
+		int item = sc.nextInt();// search wala item
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i]+" ");
+		}
+		System.out.println();
+		System.out.println(Search(arr, item));
+
+	}
+
+	public static int Search(int[] arr, int item) {
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] == item) {
+				return i;
+			}
+
+		}
+
+		return -1;
+	}
+}
