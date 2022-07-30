dffdba7d163f1e9c2749326b63b10b967b3a18f3
Aaditya kumar yadav <94703172+aditya7250@users.noreply.github.com>
2022-07-30 18:26:56 +0530

    Add files via upload

---
+++ ClassObjectEx15.java
@@ -0,0 +1,22 @@
+package CLASSandOBJECT;
+class H{
+	int x=10;
+	String y="Rahul";
+	void show() {
+		System.out.println(x+y);
+	}
+}
+public class ClassObjectEx15 {
+
+	public static void main(String[] args) {
+		H a= new H();
+		H b= new H();
+		a.x=15;
+		a.show();
+		a.y="YoYo";
+		b.show();
+		b.y="Ramu";
+		a.show();
+		b.show();
+	}
+}
