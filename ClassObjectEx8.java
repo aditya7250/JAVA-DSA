2c13db03520bd9f030960980c829a8a6dc30f3a1
Aaditya kumar yadav <94703172+aditya7250@users.noreply.github.com>
2022-07-30 18:25:32 +0530

    Add files via upload

---
+++ ClassObjectEx8.java
@@ -0,0 +1,20 @@
+package CLASSandOBJECT;
+class AA{
+	int x;
+	String y;
+	void show() {
+		System.out.println(x+y);
+	}
+}
+public class ClassObjectEx8 {
+
+	public static void main(String[] args) {
+
+		AA a=new AA();
+		a.x=10;
+		AA b=new AA();
+		b.y="Ram";
+		a.show();
+		b.show();
+	}
+}
